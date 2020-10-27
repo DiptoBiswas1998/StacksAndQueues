470a4a207cdced2edc091dba09fe46a4debd0880
unknown <diptobiswas1998@gmail.com>
2020-10-27 07:26:46 +0530

    [Dipto] refactor: Adding the welcome msg

---
+++ StacksAndQueues/StacksAndQueues/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace StacksAndQueues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Stack and Queue Problem!");
+        }
+    }
+}
